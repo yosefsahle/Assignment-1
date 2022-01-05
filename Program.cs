e423612158bdf38a1532b0ccefec436b4a7c1c29
jozi_pro <yosefsahle48@gmail.com>
2022-01-05 20:08:15 +0300

    reversing string and palindrome string

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+         static void Revers(){
+            int length;
+            string rev= "";
+            Console.WriteLine("Inpute Your String");
+            string name = Console.ReadLine();
+
+            length = name.Length -1;
+            while(length>=0)
+            {
+                rev = rev + name[length];
+                length --;
+
+            }
+            Console.WriteLine("\n_________________________________\n\n>>> the reverse of "+ name+ " is " +rev+" <<<\n_________________________________");
+            menu();
+            }
+
+            static void checkpro(){
+                int length;
+            string rev= "";
+            Console.WriteLine("Inpute Your String");
+            string name = Console.ReadLine();
+
+            length = name.Length -1;
+            while(length>=0)
+            {
+                rev = rev + name[length];
+                length --;
+
+            }
+            
+            
+            if(name==rev){
+                Console.WriteLine("\n_________________________________\n\n>>> "+name+" is palindrome String<<< \n_________________________________");
+                menu();
+            }
+            else {Console.WriteLine("\n_________________________________\n\n>>> "+name+" is not palindrome String <<< \n_________________________________");
+            menu();}
+            }
+
+            static void menu(){
+                Console.WriteLine("\nplease select number\n 1. Revers String\n 2. check palindrome\n 3. exit");
+           string N = Console.ReadLine();
+           if (N=="1"){
+               Revers();
+           }
+           else {
+               if (N == "2"){
+               checkpro();
+           }
+            else{
+                if (N == "3"){
+                    Console.WriteLine("thank You");
+                }
+                else { Console.WriteLine("\n>>>please Input the correct nember<<<\n");
+            menu();}
+            }
+           }
+            }
+        static void Main(string[] args)
+        {
+            menu();
+           
+            }
+    }
+}
