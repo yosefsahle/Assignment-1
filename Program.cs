69eda7ae47c7dec042df3ef2e34d2beaa04082eb
jozi_pro <yosefsahle48@gmail.com>
2022-01-05 21:41:08 +0300

    count occurence finished and word order revers inprogress

--- Program.cs
+++ Program.cs
@@ -53,8 +53,41 @@ namespace Assignment_1
             menu();}
             }
 
+            static void Wordor(){
+            int length;
+            string rev= "";
+            Console.WriteLine("Inpute Your Sentence");
+            string name = Console.ReadLine();
+
+            length = name.Length -1;
+            while(length>=0)
+            {
+                rev = rev + name[length];
+                length --;
+
+            }
+            Console.WriteLine("\n_________________________________\n\n>>> the reverse of "+ name+ " is " +rev+" <<<\n_________________________________");
+            menu();
+            }
+
+            static void countoccr(){
+                Console.WriteLine("Please Input Sentense or Word ...");
+                string input = Console.ReadLine();
+                Console.WriteLine("Now Insert the word you want to count");
+                string find = Console.ReadLine();
+                int count = 0;
+                foreach(char item in input){
+                    if (item == find[0]){
+                        count++;
+                    }
+                    
+                }
+                Console.WriteLine("\n__________________________\nthe Word " + find + " occurse " +count+ " times\n_____________________");
+                menu();
+            }
+
             static void menu(){
-                Console.WriteLine("\nplease select number\n 1. Revers String\n 2. check palindrome\n 3. exit");
+                Console.WriteLine("\nplease select number\n 1. Revers String\n 2. check palindrome\n 3. Reverse word Order \n 4. count Occurence of Word\n 5. exit");
            string N = Console.ReadLine();
            if (N=="1"){
                Revers();
@@ -65,6 +98,12 @@ namespace Assignment_1
            }
             else{
                 if (N == "3"){
+                    Wordor();
+                }
+                if (N == "4"){
+                    countoccr();
+                }
+                if (N == "5"){
                     Console.WriteLine("thank You");
                 }
                 else { Console.WriteLine("\n>>>please Input the correct nember<<<\n");
